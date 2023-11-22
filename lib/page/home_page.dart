c2f5ac728d3babd4e5096d18438018224f20bf08
Novan505 <nvnsty8@gmail.com>
2023-11-22 14:57:38 +0700

    Custom Component

---
+++ lib/page/home_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Home Page",
+          style: TextStyle(color: Color(0xFFFFFFFF)),
+        ),
+        backgroundColor: Colors.green.shade300,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(child: CustomMenu(imageAsset: "assets/persegii.png", title: "Persegi")),
+                  Expanded(child: CustomMenu(imageAsset: "assets/persegi pp.png", title: "Persegi Panjang")),
+            ],
+          ),
+          ),
+          Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(child: CustomMenu(imageAsset: "assets/segitigaa.png", title: "Segitiga")),
+                  Expanded(child: CustomMenu(imageAsset: "assets/lingkarann.png", title: "Lingkarann")),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAsset, required this.title,
+  });
+  final String imageAsset;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 8
+      ),
+        margin: EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 8
+      ),
+    width: 200, height: 400,
+    decoration: BoxDecoration(color: Colors.green.shade500)
+    ,child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Image.asset(imageAsset, alignment: Alignment.center, width: 200,),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(title),
+        ),
+
+      ],
+    ),
+    );
+  }
+}
